076d37c73d5f3fe0165fdb9c7686057de811f22e
Viljami Kuosmanen <viljami.kuosmanen@futurice.com>
2020-06-07 16:34:05 +0200

    Use single file instead of two

--- examples/typescript-example/src/api.ts
+++ examples/typescript-example/src/api.ts
@@ -3,15 +3,124 @@
  */
 
 import {
+  Parameters as ApiParameters,
+  UnknownParamsObject,
+  OperationResponse,
+  AxiosRequestConfig,
   OpenAPIProvider,
   useOperation as useOperationOrig,
   useOperationMethod as useOperationMethodOrig,
   UnknownOperationMethod,
   ResponseObject,
 } from 'react-openapi-client';
-import { OperationMethods, Paths } from './client';
 
-function useOperationMethod(operationId: string): [UnknownOperationMethod, ResponseObject<any>];
+declare namespace Components {
+  namespace Responses {
+    export type ListPetsRes = Schemas.Pet[];
+    export type PetRes = Schemas.Pet;
+  }
+  namespace Schemas {
+    export interface Pet {
+      id?: number;
+      /**
+       * example:
+       * Odie
+       */
+      name?: string;
+      status?: 'AVAILABLE' | 'NOT_AVAILABLE';
+      /**
+       * example:
+       * https://vignette.wikia.nocookie.net/garfield/images/a/ac/OdieCharacter.jpg/revision/latest/top-crop/width/360/height/450
+       */
+      image?: string; // uri
+    }
+  }
+}
+declare namespace Paths {
+  namespace DeletePetById {
+    namespace Parameters {
+      export type Id = number;
+    }
+    export interface PathParameters {
+      id: Parameters.Id;
+    }
+  }
+  namespace GetPetById {
+    namespace Parameters {
+      export type Id = number;
+    }
+    export interface PathParameters {
+      id: Parameters.Id;
+    }
+    namespace Responses {
+      export type $200 = Components.Responses.PetRes;
+    }
+  }
+  namespace ListPets {
+    namespace Responses {
+      export type $200 = Components.Responses.ListPetsRes;
+    }
+  }
+}
+
+export interface OperationMethods {
+  /**
+   * listPets
+   */
+  listPets(
+    parameters?: ApiParameters<UnknownParamsObject>,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.ListPets.Responses.$200>;
+  /**
+   * getPetById
+   */
+  getPetById(
+    parameters?: ApiParameters<Paths.GetPetById.PathParameters>,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.GetPetById.Responses.$200>;
+  /**
+   * deletePetById
+   */
+  deletePetById(
+    parameters?: ApiParameters<Paths.DeletePetById.PathParameters>,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<any>;
+}
+
+export interface PathsDictionary {
+  ['/pets']: {
+    /**
+     * listPets
+     */
+    get(
+      parameters?: ApiParameters<UnknownParamsObject>,
+      data?: any,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.ListPets.Responses.$200>;
+  };
+  ['/pets/{id}']: {
+    /**
+     * getPetById
+     */
+    get(
+      parameters?: ApiParameters<Paths.GetPetById.PathParameters>,
+      data?: any,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.GetPetById.Responses.$200>;
+    /**
+     * deletePetById
+     */
+    post(
+      parameters?: ApiParameters<Paths.DeletePetById.PathParameters>,
+      data?: any,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<any>;
+  };
+}
+
 function useOperationMethod(
   operationId: 'listPets',
 ): [OperationMethods['listPets'], ResponseObject<Paths.ListPets.Responses.$200>];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import { OpenAPIClientAxios, OpenAPIClient, AxiosResponse, UnknownOperationMethod } from 'openapi-client-axios';
-export { OpenAPIClientAxios, OpenAPIClient, AxiosResponse, UnknownOperationMethod };
+import {
+  OpenAPIClientAxios,
+  OpenAPIClient,
+  AxiosResponse,
+  UnknownOperationMethod,
+  Parameters,
+  UnknownParamsObject,
+  OperationResponse,
+  AxiosRequestConfig,
+} from 'openapi-client-axios';
+
 export * from './OpenAPIProvider';
 export * from './useOperationMethod';
 export * from './useOperation';
+
+export {
+  OpenAPIClientAxios,
+  OpenAPIClient,
+  AxiosResponse,
+  UnknownOperationMethod,
+  Parameters,
+  UnknownParamsObject,
+  OperationResponse,
+  AxiosRequestConfig,
+};
