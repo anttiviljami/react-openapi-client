aabc91f08649d00655200e9cdf43b90ae39e1bc9
Viljami Kuosmanen <viljami@viljami.io>
2021-12-11 22:40:49 +0100

    Release 0.2.0

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-  globals: {
-    'ts-jest': {
-      tsConfig: './tsconfig.json',
-    },
-  },
 };
